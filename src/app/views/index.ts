0d5db5ec2b1514361b3fd8e3fca03827ee11d08b
mathis-m <mathis.michel@outlook.de>
2019-07-17 22:14:58 +0100

    add builded sources

--- src/app/views/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './home/home.component';
-export * from './mirror-likes/mirror-likes.component';
-export * from './connect-spotify/connect-spotify.component';
-export * from './callback/callback.component';
